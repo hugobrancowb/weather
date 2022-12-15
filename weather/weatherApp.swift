6e5f1b4c868b6ebb3ca742dea2e14f500716807c
Hugo Branco <hugobrancowb@gmail.com>
2022-12-15 10:44:22 -0300

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Hugo Branco on 15/12/22.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
